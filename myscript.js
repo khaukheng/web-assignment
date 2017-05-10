3dda797eda4b89262a39de8323553e4b39c51d5d
khaukheng <khenggy111@gmail.com>
2017-05-10 17:33:11 +0800

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -135,7 +135,8 @@ $(document).ready(function(){
         for (var j = level + 1; j <=7 ; j++) {
             var ID2 = "pa"+j;
             var ID3 = "pb"+j;
-            document.getElementById(ID).classList.addClass("unable");
+            document.getElementById(ID2).className("unable");
+            document.getElementById(ID3).className("unable");
             document.getElementById(ID2).style.cursor="not-allowed";
             document.getElementById(ID3).style.cursor="not-allowed";
             /*document.getElementById(ID2).removeAttribute('href');
